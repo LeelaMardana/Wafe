9ae58618fe2e419ee2df2a999e51daf4f1af1057
AwesemoTowa <Deankurumi@gmail.com>
2022-11-15 01:01:51 +0600

    Add deleteData for phone

--- src/modules/deleteData.js
+++ src/modules/deleteData.js
@@ -1,12 +1,11 @@
 import { service } from './service';
 import { render } from './render';
 export const deleteData = () => {
-  const dataBody = document.querySelector('#data-body');
-
-  dataBody.addEventListener('click', e => {
-    if (e.target.closest('.btn-delete')) {
-      const id = e.target.closest('ul').querySelector('#id').textContent;
-      const elem = e.target.closest('.data__body-items');
+  const dataInner = document.querySelector('.data__inner');
+  dataInner.addEventListener('click', e => {
+    if (e.target.closest('#btn-delete')) {
+      const elem = e.target.closest('#data-item');
+      const id = elem.dataset.key;
       elem.remove();
       service
         .deleteData(id)
