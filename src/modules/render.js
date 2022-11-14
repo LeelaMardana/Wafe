cc0eca521e0686061f46884b67b148c046e7f9f3
AwesemoTowa <Deankurumi@gmail.com>
2022-11-15 00:28:43 +0600

    Add render for mobile

--- src/modules/render.js
+++ src/modules/render.js
@@ -1,13 +1,48 @@
 export const render = users => {
   const dataBody = document.querySelector('#data-body');
-
-  dataBody.innerHTML = '';
-
+  const dataItems = document.querySelector('#data-items');
   const width = document.documentElement.clientWidth;
-  console.log(width);
+
   if (width <= 1240) {
-    return;
+    dataItems.innerHTML = '';
+    users.forEach(user => {
+      dataItems.insertAdjacentHTML(
+        'beforeend',
+        `<div class="data-item" data-key="${user.id}">
+              <h4 class="data-item__title">#${user.id}</h4>
+              <ul class="data-item__list">
+                <li class="data-item__wrap">
+                  <span class="data-item__name">Имя:</span>
+                  <span class="data-item__value">${user.name}</span>
+                </li>
+                <li class="data-item__wrap">
+                  <span class="data-item__name">Телефон:</span>
+                  <span class="data-item__value">${user.phone}</span>
+                </li>
+                <li class="data-item__wrap">
+                  <span class="data-item__name">Email:</span>
+                  <span class="data-item__value">${user.email}</span>
+                </li>
+                <li class="data-item__wrap">
+                  <span class="data-item__name">Плавает:</span>
+                  <span class="data-item__value">${
+                    user.swim ? 'Да' : 'Нет'
+                  }</span>
+                </li>
+                <li class="data-item__wrap">
+                  <span class="data-item__name">Тариф:</span>
+                  <span class="data-item__value">${user.tariff}</span>
+                </li>
+                <li class="data-item__btns">
+                  <button class="data-item__btn" type="button">Изменить</button>
+                  <button class="data-item__btn" type="button">Удалить</button>
+                </li>
+              </ul>
+        </div>`
+      );
+    });
   } else {
+    dataBody.innerHTML = '';
     users.forEach(user => {
       dataBody.insertAdjacentHTML(
         'beforeend',
